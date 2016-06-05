da844e6cf895fba47097b3f6d8e78a0ee74c1aa3
Lain <lain@lain.org.uk>
2016-06-05 09:38:09 +0100

    now for actual corruption stuff

--- src/corruption.rs
+++ src/corruption.rs
@@ -1,4 +1,26 @@
 use std::io::prelude::*;
-use std::fs;
+use std::path::Path;
+use std::fs::*;
+use std::io::Result as IOResult;
 
+pub trait Mutate {
+    fn mutate_to<K: AsRef<Path>>(&mut self, out: K) -> IOResult<()>;
+}
 
+impl Mutate for File {
+    fn mutate_to<K: AsRef<Path>>(&mut self, out: K) -> IOResult<()> {
+        let mut buf = Vec::new();
+
+        for result_byte in self.bytes() {
+            let byte = try!(result_byte);
+          
+            buf.push(byte);
+        }
+        
+        let mut file = try!(File::create(&out)); {
+            try!(file.write(&buf));
+        }
+        
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod corruption;
 
 use getopts::Options;
 use std::env;
-use std::path::Path;
+use std::fs::*;
+use corruption::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,7 +14,6 @@ fn main() {
     
     options.optflag("h", "help", "print this help menu");
     options.optopt("f", "file", "input filename", "FILE");
-    options.optopt("o", "output", "output filename", "OUTPUT");
     
     let matches = match options.parse(&args[1..]) {
         Ok(m) => m,
@@ -26,8 +26,22 @@ fn main() {
         return;
     }
 
-    if matches.opt_str("f").is_none() || matches.opt_str("o").is_none() {
-        println!("Both an input filename and output filename must be provided!");
+    if matches.opt_str("f").is_none() {
+        println!("Input or output filename is missing!");
         return;
+    }
+
+    // It's safe to unwrap the option here because of the above check.
+    let mut file = match File::open(matches.opt_str("f").unwrap()) {
+        Ok(f) => f,
+        Err(_) => {
+            println!("Input file does not exist!");
+            return;
+        }
+    };
+
+    match file.mutate_to("mutated.bin") {
+        Ok(_) => println!("Successfully corrupted the file."),
+        Err(_) => println!("Failed to corrupt the file!")
     }
 }
