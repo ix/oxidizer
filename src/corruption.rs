51f7b504170bce43e534dea698266061270586c6
Lain <lain@lain.org.uk>
2016-06-07 07:02:14 +0100

    It\'s alive!

--- src/corruption.rs
+++ src/corruption.rs
@@ -13,11 +13,11 @@ pub struct Corroptions {
 
 #[allow(dead_code)]
 pub enum CorruptMethod {
-    Increment(u64),
-    Decrement(u64),
-    Multiply(u64),
-    Left(u64),
-    Right(u64),
+    Increment(u8),
+    Decrement(u8),
+    Multiply(u8),
+    Left(u32),
+    Right(u32),
     None
 }
 
@@ -33,6 +33,23 @@ impl Corroptions {
     }
 }
 
+pub trait Corrupt {
+    fn corrupt(self, method: &CorruptMethod) -> Self;
+}
+
+impl Corrupt for u8 {
+    fn corrupt(self, method: &CorruptMethod) -> Self {
+        match *method {
+            CorruptMethod::Increment(i) => self.wrapping_add(i),
+            CorruptMethod::Decrement(i) => self.wrapping_sub(i),
+            CorruptMethod::Multiply(i)  => self.wrapping_mul(i),
+            CorruptMethod::Left(i)      => self.wrapping_shl(i),
+            CorruptMethod::Right(i)     => self.wrapping_shr(i),
+            _ => self
+        }
+    }        
+}
+
 pub trait Mutate {
     fn mutate_to<K: AsRef<Path>>(&mut self, out: K, options: &Corroptions) -> IOResult<()>;
 }
@@ -40,11 +57,41 @@ pub trait Mutate {
 impl Mutate for File {
     fn mutate_to<K: AsRef<Path>>(&mut self, out: K, options: &Corroptions) -> IOResult<()> {
         let mut buf = Vec::new();
+        let mut i: u64 = 0; // track iterations
 
         for result_byte in self.bytes() {
             let byte = try!(result_byte);
-          
-            buf.push(byte);
+
+            // skip if we're not range
+            if let Some(start) = options.start {
+                if i < start {
+                    buf.push(byte);
+                    i += 1;
+                    continue;
+                }
+            }
+
+            if let Some(end) = options.end {
+                if i > end {
+                    buf.push(byte);
+                    i += 1;
+                    continue;
+                }
+            }
+
+            // skip if we're skipping
+            if let Some(skip) = options.skip {
+                if i % skip != 0 {
+                    buf.push(byte);
+                    i += 1;
+                    continue;
+                }
+            }
+
+            // nothing made us skip, ok
+            // push the corrupted byte instead
+            buf.push(byte.corrupt(&options.action));
+            i += 1;
         }
         
         let mut file = try!(File::create(&out)); {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate getopts;
+extern crate rustc_serialize;
 
 mod corruption;
 
@@ -10,12 +11,19 @@ use corruption::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let mut corrupt = Corroptions::new();
-    
+    let mut corrupt = Corroptions::new();    
     let mut options = Options::new();
     
     options.optflag("h", "help", "print this help menu");
     options.optopt("f", "file", "input filename", "FILE");
+    options.optopt("i", "increment", "increment by", "N");
+    options.optopt("d", "decrement", "decrement by", "N");
+    options.optopt("m", "multiply", "multiply by", "N");
+    options.optopt("l", "shift-left", "shift left by", "N");
+    options.optopt("r", "shift-right", "shift right by", "N");
+    options.optopt("s", "skip", "corrupt every n bytes", "N");
+    options.optopt("b", "begin", "offset to begin at", "OFFSET");
+    options.optopt("e", "end", "offset to end at", "OFFSET");
     
     let matches = match options.parse(&args[1..]) {
         Ok(m) => m,
@@ -25,6 +33,7 @@ fn main() {
     if matches.opt_present("h") {
         let brief = format!("usage: {} [options]", program);
         print!("{}", options.usage(&brief));
+        println!("Offsets are hex (prefixed with '0x')");
         return;
     }
 
@@ -33,6 +42,38 @@ fn main() {
         return;
     }
 
+    if let Some(inc) = matches.opt_str("i") {
+        corrupt.action = CorruptMethod::Increment(inc.parse::<u8>().unwrap());
+    }
+
+    if let Some(dec) = matches.opt_str("d") {
+        corrupt.action = CorruptMethod::Decrement(dec.parse::<u8>().unwrap());
+    }
+
+    if let Some(mul) = matches.opt_str("m") {
+        corrupt.action = CorruptMethod::Multiply(mul.parse::<u8>().unwrap());
+    }
+
+    if let Some(shl) = matches.opt_str("l") {
+        corrupt.action = CorruptMethod::Left(shl.parse::<u32>().unwrap());
+    }
+
+    if let Some(shr) = matches.opt_str("r") {
+        corrupt.action = CorruptMethod::Right(shr.parse::<u32>().unwrap());
+    }
+
+    if let Some(skip) = matches.opt_str("s") {
+        corrupt.skip = Some(skip.parse::<u64>().unwrap());
+    }
+
+    if let Some(start) = matches.opt_str("b") {
+        corrupt.start = Some(u64::from_str_radix(&start[2..], 16).unwrap());
+    }
+
+    if let Some(end) = matches.opt_str("e") {
+        corrupt.end = Some(u64::from_str_radix(&end[2..], 16).unwrap());
+    }
+    
     // It's safe to unwrap the option here because of the above check.
     let mut file = match File::open(matches.opt_str("f").unwrap()) {
         Ok(f) => f,
